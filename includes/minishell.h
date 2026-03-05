34b5bb7a69cbb7f6c556477ac195598aa137fa16
SonOfMaaJo <naoussiviery237@gmail.com>
2026-03-05 16:17:34 +0100

    feat : initialisation du projet avec affichage du prompt

---
+++ includes/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vnaoussi <vnaoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 15:44:47 by vnaoussi          #+#    #+#             */
+/*   Updated: 2026/03/05 16:06:19 by vnaoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <signal.h>
+# define SIG_ERROR_MSG "Error : fail to catch a signal.\n"
+
+extern int g_status;
+int    quit_error(char *msg);
+
+#endif
