23255325dd32061b4be703ade80f81aef7c442e6
vnaoussi <naoussiviery237@gmail.com>
2026-04-16 14:58:35 +0200

    fix : fix of exit function to do as bash

--- srcs/Main/exit.c
+++ srcs/Main/exit.c
@@ -30,14 +30,13 @@ int	ft_exit(t_command_ast *cmd, t_minishell_data **data)
 		return (ft_putstr_fd("exit: too many arguments.\n", 2), g_status = 1, 0);
 	if (cmd->args && ft_is_onlydigit((char *)cmd->args->content) != 1)
 		return (ft_putstr_fd("exit: non numeric arguments.\n", 2), g_status = 1, 0);
+	if (cmd->args)
+		g_status = ft_atoi((char *)cmd->args->content) % 250;
 	ft_free_command(&(*data)->cmds);
 	free_tokens(&(*data)->tokens);
 	ft_free_envs(&(*data)->envs);
 	ft_lstclear(&(*data)->execdirs, free);
 	free(*data);
-	g_status = 0;
-	if (cmd->args)
-		g_status = ft_atoi((char *)cmd->args->content) % 250;
 	printf("exit\n");
-	exit(EXIT_SUCCESS);
+	exit(g_status);
 }
